6153b22c7d057455f217fa242088669e45c1e084
slledru <slledru@msn.com>
2018-05-22 08:50:54 -0600

    initial setup for redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
 import 'bootstrap/dist/css/bootstrap.css'
+import rootReducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'))
 registerServiceWorker()
