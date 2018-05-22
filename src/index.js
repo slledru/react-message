eb39413c4632a59a2cc25c32fb7ac328f1e1c20b
slledru <slledru@msn.com>
2018-05-22 10:01:05 -0600

    upgrade App to container

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
 import './index.css'
-import App from './App'
+import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
 
 import 'bootstrap/dist/css/bootstrap.css'
